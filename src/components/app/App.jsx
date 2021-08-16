1cb1fdda65c6cb0bf5f44902c23e48c46c239065
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-16 10:45:30 -0700

    characterProvider linked and wrapped around index.js

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import CharacterList from '../characters/CharactersList';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <CharacterList />
+    </>
+  );
 }
+
