6fbaa65f80f0e1d85936347dd410607f1aa0251d
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-16 10:45:59 -0700

    content displaying on page

--- src/services/futuramaApi.js
+++ src/services/futuramaApi.js
@@ -1,9 +1,12 @@
 /* eslint-disable max-len */
 export const fetchFuturamaCharacters = async () => {
-  const res = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
-  const characters = await res.json();
+  const res = await fetch(
+    'https://futuramaapi.herokuapp.com/api/v2/characters'
+  );
+  const json = await res.json();
+  console.log(json);
 
-  return characters.map(({ PicUrl, Name }) => ({
+  return json.map(({ PicUrl, Name }) => ({
     name: Name,
     image: PicUrl,
   }));
--- src/state/CharacterProvider.jsx
+++ src/state/CharacterProvider.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { fetchFuturamaCharacters } from '../services/futuramaApi';
 
 const CharacterContext = createContext();
@@ -10,4 +10,30 @@ export const CharacterProvider = ({ children }) => {
   const apiMap = {
     futurama: fetchFuturamaCharacters,
   };
-}
+
+  useEffect(() => {
+    apiMap[selectedApi]().then(setCharacters);
+  }, [selectedApi]);
+
+  return (
+    <CharacterContext.Provider value={{ characters, setSelectedApi, apiMap }}>
+      {children}
+    </CharacterContext.Provider>
+  );
+};
+
+export const useCharacters = () => {
+  const { characters } = useContext(CharacterContext);
+  return characters;
+};
+
+export const useSetSelectedApi = () => {
+  const { setSelectedApi } = useContext(CharacterContext);
+  return setSelectedApi;
+};
+
+export const useAvailableAPIs = () => {
+  const { apiMap } = useContext(CharacterContext);
+  return Object.keys(apiMap);
+};
+
